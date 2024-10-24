73b0fcbb1ab1cb9d654a2a8308a1a900a3fbe235
Peter ONeill <peteroneilljr@gmail.com>
2024-10-24 00:14:16 -0600

    fqdn

--- 5.resources.tf
+++ 5.resources.tf
@@ -6,6 +6,6 @@ module "terraform-teleport-aws" {
   aws_vpc_id             = module.vpc.vpc_id
   aws_security_group_id  = module.vpc.default_security_group_id
   aws_subnet_id          = module.vpc.private_subnets[0]
-  teleport_proxy_address = var.teleport_proxy_address
+  teleport_proxy_address = aws_route53_record.cluster_endpoint.fqdn
   teleport_version       = var.teleport_version
 }
